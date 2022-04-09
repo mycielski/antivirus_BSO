47890c3ea4e6791b482bfb200f3786e96b3117ab
mycielski <mycielski00@gmail.com>
2022-04-09 15:35:17 +0200

    .

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -24,20 +24,28 @@ Scanner::Scanner(std::string path, std::string dbPath, HashType hashType) : data
 }
 
 std::list<File> Scanner::dirSearch(std::string &path) {
+    std::cout << "File discovery... " << std::endl;
     std::list<File> files;
     for (const fs::directory_entry &entry: fs::recursive_directory_iterator(path)) {
         if (entry.is_regular_file()) {
-            files.emplace_back(File(entry.path().string()));
-        } else if (std::regex_match(entry.path().string(), std::regex("^\\/proc"))) [[unlikely]] {
-            continue;
+            try { files.emplace_back(File(entry.path().string())); } catch (std::exception &e) {
+                std::cout << "Error: " << e.what() << std::endl;
+            }
         }
     }
+    std::cout << "Done." << std::endl;
     return files;
 }
 
 std::string quarantine_instance_setup() {
     // create new directory with its name bein the date
-    std::string quarantinePath = QUARANTINE_DIRECTORY + "/" + std::to_string(time(nullptr));
+    auto t = std::time(nullptr);
+    // create a string representation of the time in format YYYY-MM-DD--HH-MM-SS
+    auto tm = *std::localtime(&t);
+    std::stringstream ss;
+    ss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+    std::string dirName = ss.str();
+    std::string quarantinePath = QUARANTINE_DIRECTORY + "/" + dirName;
     if (mkdir(quarantinePath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
         std::cerr << "Error creating quarantine directory" << std::endl;
         exit(EXIT_FAILURE);
@@ -48,13 +56,20 @@ std::string quarantine_instance_setup() {
 }
 
 int Scanner::scan() {
-    std::string quarantine_path =  quarantine_instance_setup();
+    std::cout << "Creating quarantine directory for this scan..." << std::endl;
+    std::string quarantine_path = quarantine_instance_setup();
+    std::cout << "Quarantine directory created at " << QUARANTINE_DIRECTORY + quarantine_path << std::endl;
     if (path.empty()) [[unlikely]] {
         perror("Empty path");
         exit(EXIT_FAILURE);
     } else if (fs::is_regular_file(path)) {
+        std::cout << "Scanning file: " << path << std::endl;
         File file(path);
         file.setMalicious(database.checkHash(file.getSha256()));
+        if (not file.isMalicious()) {
+            std::cout << "File is not malicious" << std::endl;
+            return 0;
+        }
         std::cout << file.getPath() << " is malicious. Its hash is " << file.getSha256() << std::endl;
         std::cout << "Quarantining file... ";
         // create a symlink from discovered malicious file to this scan's quarantine directory
@@ -65,12 +80,11 @@ int Scanner::scan() {
         std::cout << "Done. " << std::endl;
         return 1;
     }
-    std::cout << "File discovery... ";
     auto files = dirSearch(path);
-    std::cout << "Done. " << std::endl;
     int malicious_files = 0;
     std::cout << "Scanning " << files.size() << " files..." << std::endl;
     for (auto file: files) {
+        std::cout << "Scanning " << file.getPath() << "..." << std::endl;
         file.setMalicious(database.checkHash(file.getSha256()));
         if (file.isMalicious()) [[unlikely]] {
             std::cout << file.getPath() << " is malicious. Its hash is: " << file.getSha256() << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 
+#include <iostream>
 #include "Scanner.h"
 #include "setup.h"
 
 int main(int argc, char *argv[]) {
-    auto path = "../";
+    auto path = "/";//argv[0];
+    auto dbPath = argv[1];
     auto hashType = HashType::SHA256;
-    auto dbPath = "../examples/hashes";
     create_quarantine_dir();
+    std::cout << "Reading hashes from " << dbPath << "... ";
     Scanner scanner(path, dbPath, hashType);
+    std::cout << "Done." << std::endl;
+    std::cout << "Commencing scan of " << path << std::endl;
     scanner.scan();
     return 0;
 }
