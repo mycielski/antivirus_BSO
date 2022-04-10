68d7fb7fb2c665d340e95127b09a128227adcde2
mycielski <mycielski00@gmail.com>
2022-04-10 14:28:02 +0200

    update build script to generate a cleanup script

--- build.sh
+++ build.sh
@@ -3,5 +3,18 @@ cd ./doc || exit
 ./build_manpage.sh
 cd ..
 cmake . -DCMAKE_BUILD_TYPE=Release
-make
-mv ./antivirus_BSO ./avbso
+make || exit
+mkdir ./build
+mkdir ./build/release
+mv ./antivirus_BSO ./build/release/avbso
+
+echo "Creating cleanup script "
+echo '''#!/bin/bash''' > ./cleanup.sh
+echo '''rm -rf ./build''' >> ./cleanup.sh
+echo '''rm -rf ./cmake-build-debug''' >> ./cleanup.sh
+echo '''rm -rf ./CmakeFiles''' >> ./cleanup.sh
+echo '''rm CMakeCache.txt''' >> ./cleanup.sh
+echo '''rm cmake_install.cmake''' >> ./cleanup.sh
+echo '''rm Makefile''' >> ./cleanup.sh
+echo '''rm cleanup.sh''' >> ./cleanup.sh
+chmod +x cleanup.sh
